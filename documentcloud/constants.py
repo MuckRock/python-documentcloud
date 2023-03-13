7aac88131abb12930d9134ae4ef50c82b796d931
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-03-13 17:59:37 -0500

    Bumping timeout so that patch() performs better
    patch() call seems to be hitting some timeout issues in a few Add-Ons
    https://github.com/MuckRock/documentcloud-bulk-edit-addon/actions/runs/4409251880/jobs/7725303542
    https://github.com/MuckRock/documentcloud-move-account-addon/actions/runs/4390883469/jobs/7689653040
    I see patch() defined here from a more generic method
    https://github.com/MuckRock/python-documentcloud/blob/master/documentcloud/client.py
    where TIMEOUT is imported from constants, where it is defined as 10 here. I've changed it 20 to see if Add-On failure rate drops.

--- documentcloud/constants.py
+++ documentcloud/constants.py
@@ -5,6 +5,6 @@ PER_PAGE_MAX = 100
 BULK_LIMIT = 25
 BASE_URI = "https://api.www.documentcloud.org/api/"
 AUTH_URI = "https://accounts.muckrock.com/api/"
-TIMEOUT = 10
+TIMEOUT = 20
 RATE_LIMIT = 10
 RATE_PERIOD = 1
