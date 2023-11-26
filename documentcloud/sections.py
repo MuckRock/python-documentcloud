1e6bf372d23a5a51f20d0cce92963ffc771fcac3
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-11-26 08:39:39 -0600

    Reformat sections.py

--- documentcloud/sections.py
+++ documentcloud/sections.py
@@ -1,31 +1,22 @@
-# Future
-from __future__ import division, print_function, unicode_literals
-
-# Third Party
-from future.utils import python_2_unicode_compatible
-
 # Local
 from .base import BaseAPIObject, ChildAPIClient
 from .toolbox import merge_dicts
 
-
-@python_2_unicode_compatible
 class Section(BaseAPIObject):
     """A section of a document"""
 
     writable_fields = ["page_number", "title"]
 
     def __str__(self):
-        return "{} - p{}".format(self.title, self.page)
+        return f"{self.title} - p{self.page}"
 
     @property
     def api_path(self):
-        return "documents/{}/sections".format(self.document.id)
+        return f"documents/{self.document.id}/sections"
 
     @property
     def page(self):
         return self.page_number
-
 
 class SectionClient(ChildAPIClient):
     """Client for interacting with Sections"""
@@ -34,11 +25,12 @@ class SectionClient(ChildAPIClient):
 
     @property
     def api_path(self):
-        return "documents/{}/sections".format(self.parent.id)
+        return f"documents/{self.parent.id}/sections"
 
     def create(self, title, page_number):
         data = {"title": title, "page_number": page_number}
-        response = self.client.post(self.api_path + "/", json=data)
+        response = self.client.post(f"{self.api_path}/", json=data)
         return Section(
             self.client, merge_dicts(response.json(), {"document": self.parent})
         )
+
